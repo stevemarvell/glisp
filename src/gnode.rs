d32e8a69b9a7c12d82b9a6c76b4d9a0fe0d6b6f7
Steve Marvell <stevemarvell@crucialtechnical.com>
2023-02-20 21:50:06 +0000

    change to indexed

---
+++ src/gnode.rs
@@ -0,0 +1,60 @@
+use std::fmt;
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
+pub struct Gnode {
+    pub id: u64,
+    pub node_type: NodeType,
+}
+
+impl Gnode {
+    pub fn new(id: u64, node_type: NodeType) -> Gnode {
+        Gnode {
+            id: id,
+            node_type: node_type,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
+pub enum NodeType {
+    N(i64),
+    Math(MathOp),
+    Cmp(CmpOp),
+    Cond,
+}
+
+#[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
+pub enum MathOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+#[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
+pub enum CmpOp {
+    Lt,
+    Le,
+    Eq,
+    Ge,
+    Gt,
+    Ne,
+}
+
+impl MathOp {
+    pub fn to_str(&self) -> &'static str {
+        match self {
+            MathOp::Add => "+",
+            MathOp::Sub => "-",
+            MathOp::Mul => "*",
+            MathOp::Div => "/",
+        }
+    }
+}
+
+impl std::fmt::Display for MathOp {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_str())
+    }
+}
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,103 @@
 use std::collections::HashMap;
 
-#[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
-pub enum Gnode {
-    N(i64),
-}
+use gnode::*;
+
+pub mod gnode;
 
 #[derive(Debug)]
 pub struct Glisp {
-    gnodes: HashMap<Gnode, Vec<Gnode>>,
+    nodes: HashMap<Gnode, Vec<Gnode>>,
+    next_id: u64,
 }
 
 impl Glisp {
     pub fn new() -> Glisp {
-        Glisp { gnodes: HashMap::new() }
+        Glisp {
+            nodes: HashMap::new(),
+            next_id: 0,
+        }
     }
 
-    pub fn add_gnode(&mut self, gnode: Gnode) {
-        self.gnodes.entry(gnode).or_insert(Vec::new());
+    pub fn add_node<T: Into<NodeType>>(&mut self, node_type: T) -> Gnode {
+        let id = self.next_id;
+        let nt = node_type.into();
+        let gnode = Gnode::new(id, nt);
+        self.next_id += 1;
+        gnode
     }
 
-    pub fn add_link(&mut self, from: Gnode, to: Gnode) {
-        self.gnodes.entry(from).or_insert(Vec::new()).push(to);
+    pub fn add_link(&mut self, from: Gnode, to: &[Gnode]) {
+        let nodes = self.nodes.entry(from).or_insert(Vec::new());
+        nodes.extend(to.iter().cloned());
     }
 }
+
+impl Glisp {
+    pub fn to_lisp(&self, gnode: &Gnode) -> String {
+        match gnode.node_type {
+            NodeType::N(n) => n.to_string(),
+            NodeType::Math(op) => {
+                let children = self.nodes.get(gnode).unwrap();
+                let child1 = self.to_lisp(&children[0]);
+                let child2 = self.to_lisp(&children[1]);
+                format!("({} {} {})", op, child1, child2)
+            }
+            NodeType::Cmp(op) => {
+                let children = self.nodes.get(gnode).unwrap();
+                let child1 = self.to_lisp(&children[0]);
+                let child2 = self.to_lisp(&children[1]);
+                format!("({:?} {} {})", op, child1, child2)
+            }
+            NodeType::Cond => {
+                let children = self.nodes.get(gnode).unwrap();
+                let condition = self.to_lisp(&children[0]);
+                let then_link = self.to_lisp(&children[1]);
+                let else_link = self.to_lisp(&children[2]);
+                format!("(? {} {} {})", condition, then_link, else_link)
+            }
+        }
+    }
+}
+
+impl Glisp {
+    pub fn evaluate(&self, gnode: &Gnode) -> i64 {
+        match gnode.node_type {
+            NodeType::N(n) => n,
+            NodeType::Math(op) => {
+                let children = self.nodes.get(gnode).unwrap();
+                let child1 = self.evaluate(&children[0]);
+                let child2 = self.evaluate(&children[1]);
+                match op {
+                    MathOp::Add => child1 + child2,
+                    MathOp::Sub => child1 - child2,
+                    MathOp::Mul => child1 * child2,
+                    MathOp::Div => child1 / child2,
+                }
+            }
+            NodeType::Cmp(op) => {
+                let children = self.nodes.get(gnode).unwrap();
+                let child1 = self.evaluate(&children[0]);
+                let child2 = self.evaluate(&children[1]);
+                match op {
+                    CmpOp::Lt => (child1 < child2) as i64,
+                    CmpOp::Le => (child1 <= child2) as i64,
+                    CmpOp::Eq => (child1 == child2) as i64,
+                    CmpOp::Ge => (child1 >= child2) as i64,
+                    CmpOp::Gt => (child1 > child2) as i64,
+                    CmpOp::Ne => (child1 != child2) as i64,
+                }
+            }
+            NodeType::Cond => {
+                let children = self.nodes.get(gnode).unwrap();
+                let condition = self.evaluate(&children[0]);
+                if condition != 0 {
+                    self.evaluate(&children[1])
+                } else {
+                    self.evaluate(&children[2])
+                }
+            }
+        }
+    }
+}
+
 
